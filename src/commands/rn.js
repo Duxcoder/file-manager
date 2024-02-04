4a476025ce7b6fcd76ef4c9847e13ff43edcfb3c
Duxcoder <iinegve_25@mail.ru>
2024-02-04 21:45:37 +0300

    fix: fix rm command

--- src/commands/rn.js
+++ src/commands/rn.js
@@ -1,10 +1,11 @@
 import { rename } from 'node:fs/promises';
 import { resolve } from 'node:path';
+import { getNameFile } from '../utils/index.js';
 
 export default async function runRn([oldPath, newPath]) {
   try {
     const oldName = resolve(process.cwd(), oldPath);
-    const newName = resolve(process.cwd(), newPath);
+    const newName = oldName.replace(getNameFile(oldName), getNameFile(newPath));
     await rename(oldName, newName);
     console.log('file renamed successfully');
   } catch (error) {
