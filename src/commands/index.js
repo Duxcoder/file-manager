6cab6ee21dac95ca9309530955f663402a4c2655
Duxcoder <iinegve_25@mail.ru>
2024-02-04 11:35:57 +0300

    feat: add runLs

--- src/commands/index.js
+++ src/commands/index.js
@@ -7,3 +7,22 @@ const currentDirname = new CurrentDirectory();
 
 export const runUp = () => currentDirname.set(dirname(currentDirname.get()));
 export const runCd = (path) => currentDirname.set(resolve(currentDirname.get(), path));
+export const runLs = async () => {
+  const fileList = [];
+  try {
+    const files = await readdir(currentDirname.get());
+
+    for (const file of files) {
+      const filePath = resolve(currentDirname.get(), file);
+      const fileStats = await stat(filePath);
+      const fileItem = {
+        Name: file,
+        Type: fileStats.isDirectory() ? 'directory' : 'file',
+      };
+      fileList.push(fileItem);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+  console.table(fileList);
+};
