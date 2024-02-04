05d6079500a422a24a121d8926d06300702cfe4d
Duxcoder <iinegve_25@mail.ru>
2024-02-04 11:35:24 +0300

    feat: add runCd

--- src/commands/index.js
+++ src/commands/index.js
@@ -6,3 +6,4 @@ import CurrentDirectory from '../currentDirectory.js';
 const currentDirname = new CurrentDirectory();
 
 export const runUp = () => currentDirname.set(dirname(currentDirname.get()));
+export const runCd = (path) => currentDirname.set(resolve(currentDirname.get(), path));
